f9319abb28371a1f784e76cecafb0afcbf668449
Evan Hildreth <me@eph.me>
2026-05-11 21:35:11 -0400

    Maybe start config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,36 @@
 // @ts-check
+import sveltiaCms from 'astro-loader-sveltia-cms';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		sveltiaCms({
+      config: {
+        backend: {
+          name: "github",
+          repo: "oddevan/madcrasher-astro",
+          branch: "main",
+        },
+        media_folder: "src/images",
+        collections: [
+          {
+            name: "albums",
+            folder: "src/albums",
+            fields: [
+              { name: "title", widget: "string" },
+              { name: "draft", widget: "boolean", required: false },
+              { name: "date", widget: "datetime" },
+							{ name: "cover", widget: "image" },
+              { name: "body", widget: "markdown" },
+							{ name: "links", widget: "list", fields: [
+								{ name: "label", widget: "string" },
+								{ name: "url", widget: "url" },
+							]},
+            ],
+          },
+        ],
+      },
+		})
+	]
+});
